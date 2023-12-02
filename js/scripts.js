aae662ed193ae9242f45e4c6dc5174e8e3267ab1
timsdesigns <tims.designs@yahoo.com>
2023-12-02 07:26:56 +0100

    Update scripts.js
    Added pokemonList and populated it with 4 objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,25 @@
-alert('Hello World');
-
-let favoriteFood = "Panzerotti";
-document.write(favoriteFood);
+let pokemonList = [];
+pokemonList[0] = {
+ name: "Bulbasaur",
+ height : 0.7 ,
+ weight : 6.9 ,
+ types: ['grass', 'poison']
+}
+pokemonList[1] = {
+ name: "Pidgey",
+ height : 0.3 ,
+ weight : 1.8 ,
+ types: ['flying', 'normal']
+}
+pokemonList[2] = {
+ name: "Pikachu",
+ height : 0.4 ,
+ weight : 6 ,
+ types: ['electric']
+}
+pokemonList[3] = {
+ name: "Umbreon",
+ height : 1 ,
+ weight : 27 ,
+ types: ['dark']
+}

e3adbd1c527dade9399241d878720b9c6cbb7e36
timsdesigns <tims.designs@yahoo.com>
2023-12-29 05:01:42 +0100

    Update scripts.js
     - removed dormant code
     - added filter as find function
     - added small functional test

--- js/scripts.js
+++ js/scripts.js
@@ -25,16 +25,24 @@ let pokemonRepository = (function(){
     types: ['dark']
   }
   function getAll(){ return pokemonList; } 
-  // function add(pokemon){ pokemonList.push(pokemon); }
   let add = pokemon => typeof(pokemon) === 'object'?
     pokemonList.push(pokemon): reportError('Wrong data type added, try using an object.');
+  function find(pokemon) {
+    return pokemonList.filter((p) => p.name.toLowerCase().includes(pokemon.toLowerCase()));
+  }
   return {
     getAll: getAll,
-    add: add
+    add: add,
+    find: find
   }
 })();
+
+// Functional tests:
 // pokemonRepository.add(pokemonRepository.getAll()[3]); // Test adding (should work)
 // pokemonRepository.add('beer'); // Test adding string (shouldn't work, but print error)
+// console.log(pokemonRepository.find('Umbreon')); // Test find (should work)
+
+// Content to page
 // Preparing object-list for page and highlight outlier:
 let pDocList = `<div class="pokemonList">\n<h1>Pokemon List</h1>\n<ul>`; // Title
 pokemonRepository.getAll().forEach(pokemon => {

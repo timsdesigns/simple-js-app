62d8156266de469f4db2bbd12e90edfe6ad68b42
timsdesigns <tims.designs@yahoo.com>
2024-01-27 15:12:55 +0100

    Update scripts.js
     - Implementing Carousel pattern
     - Adding comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
 let pokemonRepository = (() => {
   let pokemonList = []; // Creating list for custom objects
-  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150'; // To fetch name and detailsURL 
 
   let add = pokemon => typeof (pokemon) === 'object' ?
-    pokemonList.push(pokemon) : reportError('Wrong data type added, try using an object.');
+    pokemonList.push(pokemon) : reportError('Wrong data type added, try using an object.'); // Add one pokemon object
   let getAll = () => pokemonList;
-  let find = pokemon =>
-    pokemonList.filter((p) => p.name.toLowerCase().includes(pokemon.toLowerCase()));
+  let find = pokemonName =>
+    pokemonList.filter((p) => p.name.toLowerCase().includes(pokemonName.toLowerCase()))[0]; // Find 1st pokemon object by name
+  let findIndex = pokemonName => pokemonList.indexOf(find(pokemonName)); // Get Index in pokemonList by name
+
   // Adding buttons as list items to page per pokemon
-  let buttonClickHandler = (button, p) => button.addEventListener('click', () => showDetails(p));
   let addListItem = pokemon => {
     let pokList = document.querySelector(".pokemon-list");
     let listItem = document.createElement("li"); // Adding to DOM
@@ -19,16 +20,18 @@ let pokemonRepository = (() => {
     pokList.appendChild(listItem);
     buttonClickHandler(button, pokemon);
   }
+  // Function to handle click events on the list buttons
+  let buttonClickHandler = (button, p) => button.addEventListener('click', () => showDetails(p));
 
   // Load data from an external source:
   // GET shorthand via fetch promise, then parse to object as promise via .json(),
   // then access collection ('results' key as defined per payload),
   // and add each object via add() using properties as in payload
   let LoadList = () => {
-    showLoadingMessage();
-    return fetch(apiUrl).then(res => res.json())
+    showLoadingMessage(); // Show the loading message
+    return fetch(apiUrl).then(res => res.json()) /* Fetch the list of pokemon from the API */
       .then(o => o.results.forEach(p =>
-        add({ name: p.name, detailsUrl: p.url })
+        add({ name: p.name, detailsUrl: p.url }) /* Add each pokemon to the pokemonList */
       )).catch(e => console.error(e))
       .finally(() => hideLoadingMessage());
   }
@@ -40,17 +43,17 @@ let pokemonRepository = (() => {
     return fetch(p.detailsUrl).then(res => res.json())
       .then(d => {
         p.imgUrl = d.sprites.front_default,
-          p.height = d.height,
-          p.weight = d.weight,
-          p.moves = d.moves.map(m => m.move.name).join(", "),
-          p.types = d.types.map(t => t.type.name).join(", ")
+        p.height = d.height,
+        p.weight = d.weight,
+        p.moves = d.moves.map(m => m.move.name).join(", "),
+        p.types = d.types.map(t => t.type.name).join(", ")
       }).catch(e => console.error(e))
       .finally(() => hideLoadingMessage());
   }
 
   let hideModal = () => document.querySelector("#modal-container").classList.remove("is-visible");
   let showModal = (title, text, extra = null) => {
-    // Make modal elements (in box)
+    // Create modal elements (in box)
     let closeButtonElement = document.createElement("button");
     closeButtonElement.innerText = "Close";
     closeButtonElement.classList.add("modal-close");
@@ -59,6 +62,7 @@ let pokemonRepository = (() => {
     let modalText = document.createElement("p");
     modalText.innerText = text;
     let modalElements = [closeButtonElement, modalTitle, modalText];
+    // Add details as extra elements
     if (extra) {
       let modalExtra = document.createElement("div");
       modalExtra.appendChild(extra);
@@ -76,17 +80,18 @@ let pokemonRepository = (() => {
     modalContainer.appendChild(modal); //Add new modal box
     modalContainer.classList.add("is-visible"); //Show
 
+    // Add event listeners to the close button and the modal container
     closeButtonElement.addEventListener("click", hideModal); //Hide when button clicked
     modalContainer.addEventListener("click", e => { if (e.target === modalContainer) hideModal() }) //Hide when clicked
     window.addEventListener("keydown", e => { if (e.key === "Escape") hideModal() }); //Hide when Esc
   }
-  // Load from API instead of static data
+  // Function to show the details of a pokemon in the modal
   // - call loadDetails(), pass Pokémon object as parameter
   // - display in modal interface
   let showDetails = p => {
-    loadDetails(p).then(() => {
-      let extraHTML = document.createElement("ul");
-
+    loadDetails(p).then(() => { /* Load the details of the pokemon */
+      let extraHTML = document.createElement("ul"); // Create the elements for the details
+      // Add each detail to the list
       Object.keys(p).forEach(k => {
         let li = document.createElement("li");
         li.classList.add("details-item");
@@ -95,7 +100,7 @@ let pokemonRepository = (() => {
         title.innerText = k.includes("detailsUrl") ? "source" : k;
         if (k === "name") {
           return;
-        }else if (k === "imgUrl") {
+        } else if (k === "imgUrl") {
           title.innerText = "";
           let image = document.createElement("img");
           image.src = p[k];
@@ -127,10 +132,28 @@ let pokemonRepository = (() => {
         extraHTML.appendChild(title);
         extraHTML.appendChild(li);
       });
-
-      showModal(p.name, "Details:", extraHTML);
+      showModal(p.name, "Details:", extraHTML); // Show the modal with the details
     });
   }
+
+  //#region Carousel
+  // Swipe detection
+  let startX = 0;
+  let container = document.querySelector("#modal-container");
+  container.addEventListener("touchstart", e => startX = e.changedTouches[0].screenX);
+  container.addEventListener("touchend", e => updateCarousel(e.changedTouches[0].screenX - startX));
+  // Slide display
+  let updateCarousel = (change) => {
+    let currentPokemonName = document.querySelector("#modal-container > .modal > h1").innerText;
+    let currentIndex = findIndex(currentPokemonName); // Get index in pokemonList
+    let nextIndex = (currentIndex + 1) % getAll().length; // Calc next, wrap end
+    let prevIndex = (currentIndex - 1 + getAll().length) % getAll().length; // Calc previous in wrapped list
+    if (change > 50) loadDetails(getAll()[prevIndex])
+      .then(() => showDetails(getAll()[prevIndex])); // Get previous on swipe left if found/loading resolved
+    else if (change < -50) loadDetails(getAll()[nextIndex])
+      .then(() => showDetails(getAll()[nextIndex])); // Get next on swipe right
+  }
+  //#endregion Carousel
 
   // Displaying message while data is being loaded
   // - Implement showLoadingMessage() and hideLoadingMessage() to append/remove a message to the page
@@ -153,14 +176,15 @@ let pokemonRepository = (() => {
     const loadingElement = document.getElementById('loading-message');
     if (loadingElement) loadingElement.remove();
   }
-
+  // Return the public functions
   return {
     add: add,
     getAll: getAll,
     find: find,
+    findIndex: findIndex,
     addListItem: addListItem,
     LoadList: LoadList,
-    loadDetails: loadDetails
+    loadDetails: loadDetails,
   }
 })();
 

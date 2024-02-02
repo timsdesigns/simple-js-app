5e52c0721b411b56b7519499ad3a333035dc3545
timsdesigns <tims.designs@yahoo.com>
2024-02-02 18:12:38 +0100

    Cleanup
    Old CSS and JavaScript code has been removed

--- js/scripts.js
+++ js/scripts.js
@@ -20,38 +20,34 @@
     let pokemonList = []; // Creating list for custom objects
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150'; // To fetch name and detailsURL 
 
+    //#region Utility functions
     let add = pokemon => typeof (pokemon) === 'object' ?
       pokemonList.push(pokemon) : reportError('Wrong data type added, try using an object.'); // Add one pokemon object
     let getAll = () => pokemonList;
     let find = pokemonName =>
       pokemonList.filter((p) => p.name.toLowerCase().includes(pokemonName.toLowerCase()))[0]; // Find 1st pokemon object by name
     let findIndex = pokemonName => pokemonList.indexOf(find(pokemonName)); // Get Index in pokemonList by name
+    //#endregion Utility functions
 
-    // Adding buttons as list items to page per pokemon
+    //#region Fill page
+    // Adding buttons as divs to page per pokemon
     let addListItem = pokemon => {
       let pokList = document.querySelector(".pokemon-list");
       let listItem = document.createElement("div"); // Adding to DOM
       listItem.className = "col-sm";
       let button = document.createElement("button");
       button.innerText = pokemon.name;
-      //button.classList.add("list-button");
-      //button.classList.add("btn");  // Adding Bootstrap
-      //button.classList.add("list-group-item", "badge", "bg-primary", "rounded-pill");  // Adding Bootstrap
-      //button.classList.add("d-flex", "align-items-center", "py-2");  // Adding Bootstrap
       button.classList.add(
         "btn", "btn-primary", "rounded-pill", "text-white", "my-2", "fw-bold",
         "px-3", "btn-fixed-min-width");
       listItem.appendChild(button);
       pokList.appendChild(listItem);
-      // Bootstrap version
-      //buttonClickHandler(button, pokemon);
       button.classList.add("btn-primary");
       button.setAttribute("data-bs-toggle", "modal");
       button.setAttribute("data-bs-target", "#exampleModal");
       buttonClickHandlerBs(button, pokemon);
     }
     // Function to handle click events on the list buttons
-    let buttonClickHandler = (button, p) => button.addEventListener('click', () => showDetails(p));
     let buttonClickHandlerBs = (button, p) => button.addEventListener('click', () => showDetailsBs(p));
 
     // Load data from an external source:
@@ -66,7 +62,9 @@
         )).catch(e => console.error(e))
         .finally(() => hideLoadingMessage());
     }
+    //#endregion Fill page
 
+    //#region Details
     // Request details on p object, then parse to details object, 
     // then add chosen details properties back to p object (map instead forEach)
     let loadDetails = p => {
@@ -82,42 +80,7 @@
         .finally(() => hideLoadingMessage());
     }
 
-    let hideModal = () => document.querySelector("#modal-container").classList.remove("is-visible");
-    let showModal = (title, text, extra = null) => {
-      // Create modal elements (in box)
-      let closeButtonElement = document.createElement("button");
-      closeButtonElement.innerText = "Close";
-      closeButtonElement.classList.add("modal-close");
-      let modalTitle = document.createElement("h1");
-      modalTitle.innerText = title;
-      let modalText = document.createElement("p");
-      modalText.innerText = text;
-      let modalElements = [closeButtonElement, modalTitle, modalText]; // button, h1, p
-      // Add details as extra elements
-      if (extra) {
-        let modalExtra = document.createElement("div"); // div with 
-        modalExtra.appendChild(extra);
-        modalElements.push(modalExtra);
-      }
-
-      // Add elements to modal box (on top of its container)
-      let modal = document.createElement("div");
-      modalElements.forEach(element => modal.appendChild(element));
-      modal.classList.add("modal");
-
-      // Add modal to modal-container (clickable around modal box)
-      let modalContainer = document.querySelector("#modal-container");
-      //modalContainer.innerHTML = ""; //Clear existing modals
-      modalContainer.appendChild(modal); //Add new modal box
-      modalContainer.classList.add("is-visible"); //Show
-
-      // Add event listeners to the close button and the modal container
-      closeButtonElement.addEventListener("click", hideModal); //Hide when button clicked
-      modalContainer.addEventListener("click", e => { if (e.target === modalContainer) hideModal() }) //Hide when clicked
-      window.addEventListener("keydown", e => { if (e.key === "Escape") hideModal() }); //Hide when Esc
-    }
-
-    // Functions to show the Bootstrap version
+    // Functions to show the Bootstrap modal
     let showModalBS = (title, text, extra = null) => {
       // Top row in modal box
       let modalHeaderBs = document.querySelector(".modal-header");
@@ -199,7 +162,6 @@
                 truncatedText.classList.add("rounded-pill");
                 toggleFullText = true;
               }
-              //buttonWrap.style.display = 'none';
             });
 
             div.appendChild(buttonWrap);
@@ -215,59 +177,7 @@
         showModalBS(p.name, "Details:", extraHTML);
       });
     }
-
-    // Function to show the details of a pokemon in the modal
-    // - call loadDetails(), pass Pokémon object as parameter
-    // - display in modal interface
-    let showDetails = p => {
-      loadDetails(p).then(() => { /* Load the details of the pokemon */
-        let extraHTML = document.createElement("ul"); // Create the elements for the details
-        // Add each detail to the list
-        Object.keys(p).forEach(k => {
-          let li = document.createElement("li");
-          li.classList.add("details-item");
-          let title = document.createElement("h3");
-          title.classList.add("details-title");
-          title.innerText = k.includes("detailsUrl") ? "source" : k;
-          li.appendChild(title);
-
-          if (k === "name") {
-            return;
-          } else if (k === "imgUrl") {
-            title.innerText = "";
-            let image = document.createElement("img");
-            image.src = p[k];
-            image.alt = "image";
-            li.appendChild(image);
-          } else if (k === "moves") {
-            // replace preview if clicked
-            let div = document.createElement("div");
-            let paragraph = document.createElement("p");
-            let truncatedText = p[k].split(',').slice(0, 10).join(', ') + '...';
-            paragraph.innerText = truncatedText;
-            div.appendChild(title);
-            div.appendChild(paragraph);
-            let button = document.createElement("a");
-            button.innerText = "Show all";
-            button.href = "#";
-            button.addEventListener('click', e => {
-              e.preventDefault();
-              paragraph.innerText = p[k];
-              button.style.display = 'none';
-            });
-            div.appendChild(button);
-            li.appendChild(div);
-          } else {
-            let paragraph = document.createElement("p");
-            paragraph.innerText = p[k];
-            paragraph.classList.add("details-content");
-            li.appendChild(paragraph);
-          }
-          extraHTML.appendChild(li);
-        });
-        showModal(p.name, "Details:", extraHTML); // Show the modal with the details
-      });
-    }
+    //#endregion Details
 
     //#region Carousel
     // Swipe detection
@@ -295,6 +205,7 @@
     }
     //#endregion Carousel
 
+    //#region Loading message
     // Displaying message while data is being loaded
     // - Implement showLoadingMessage() and hideLoadingMessage() to append/remove a message to the page
     // - In LoadList() and loadDetails(); showLoadingMessage() should be the first executed call
@@ -316,6 +227,8 @@
       const loadingElement = document.getElementById('loading-message');
       if (loadingElement) loadingElement.remove();
     }
+    //#endregion Loading message
+
     // Return the public functions
     return {
       add: add,
